1f2e598000d786f15b215836d54dbdc165fa1ffc
Jean-Luc Bouchot <jlbouchot@gmail.com>
2020-03-20 03:08:51 +0800

    Adds the base class for numerical algorithms

---
+++ Algos/NumericalAlgos.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+__author__ = ["Jean-Luc Bouchot"]
+__copyright__ = "Jean-Luc Bouchot"
+__credits__ = ["Jean-Luc Bouchot"]
+__license__ = "GPLv3"
+__version__ = "0.1.0-dev"
+__maintainer__ = "Jean-Luc Bouchot"
+__email__ = "jlbouchot@gmail.com"
+__status__ = "Development"
+__lastmodified__ = "2020/03/19"
+__created__ = "2020/03/19"
+
+class NumericalSolver:
+    """Generic class for a numerical solver."""
+    def __init__(self, anOperator): 
+        self.lhs = anOperator
+        self.iterates = [] # This will contain the various estimations, in case of iterative algorithms
+
+    def solve(self):
+        return self.solve()
+
