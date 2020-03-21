26eec6e1016b5b647eed98fdaecb13d13c549d64
Jean-Luc Bouchot <jlbouchot@gmail.com>
2020-03-21 19:01:05 +0800

    Adds init file for considering the whole repo as a package

---
+++ __init__.py
@@ -0,0 +1,11 @@
+__author__ = "Jean-Luc Bouchot"
+__copyright__ = "Copyright 2020, Jean-Luc Bouchot"
+__credits__ = "Jean-Luc Bouchot"
+__license__ = "GPLv3"
+__version__ = "0.1.0-dev"
+__maintainer__ = "Jean-Luc Bouchot"
+__email__ = "jlbouchot@gmail.com"
+__status__ = "Development"
+__lastmodified__ = "2020/03/21"
+__created__ = "2020/03/21"
+
