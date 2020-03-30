cf30a25c1f1617b027c929d021362860cc50de47
Jean-Luc Bouchot <jlbouchot@gmail.com>
2020-03-30 22:32:59 +0100

    Adapts again the handling of arguments

--- Algos/RootFindingAlgos/NewtonsMethod.py
+++ Algos/RootFindingAlgos/NewtonsMethod.py
@@ -10,7 +10,7 @@ __version__ = "0.1.0-dev"
 __maintainer__ = "Jean-Luc Bouchot"
 __email__ = "jlbouchot@gmail.com"
 __status__ = "Development"
-__lastmodified__ = "2020/03/29"
+__lastmodified__ = "2020/03/30"
 __created__ = "2020/03/29"
 
 class NewtonsIterations1D(RootFindingAlgos): 
