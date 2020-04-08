e6f90f8f5f8f76920f5455a97bcd36eec61de87c
Jean-Luc Bouchot <jlbouchot@gmail.com>
2020-04-08 21:04:34 +0100

    Adds LMA with non identity diagonal matrix

--- Algos/OptimisationAlgos/LMA.py
+++ Algos/OptimisationAlgos/LMA.py
@@ -110,3 +110,55 @@ class ClassicalLM(NumericalAlgos):
 
         return xNew
 
+
+class SndOrderLM(NumericalAlgos): 
+    '''Levenberg-Marquardt algorithm with second order damping''' 
+
+    def __init__(self, anOperator, x0, aJacobian, nbIter, omega0):  
+        super().__init__(anOperator)
+
+        self.x0 = x0
+        self.nbIter = nbIter 
+
+        self.J = aJacobian
+
+        self.d = 1 if type(x0) == float else len(x0)
+
+        self.algoName = "Second order LM optimisation"
+
+        curValue = self.lhs(x0)
+
+        self.estimates = np.zeros([self.d,nbIter+1])
+        self.estimates[:,0] = x0
+
+        self.residuals = np.zeros([len(curValue), nbIter+1])
+        self.residuals[:,0] = curValue
+
+        self.errors = 100000*np.ones([nbIter+1])
+        self.errors[0] = np.linalg.norm(curValue)
+
+        self.omegas = np.zeros([nbIter+1])
+        self.omegas[0] = omega0
+
+    def solve(self): 
+        xOld = self.x0
+        for oneiter in range(0,self.nbIter) : 
+            curGradient = self.J(xOld)
+            curHessian = np.matmul(curGradient.transpose(),curGradient)
+            xNew = xOld - np.dot(np.linalg.pinv(curHessian + self.omegas[oneiter]*np.diag(np.diag(curHessian))),np.matmul(curGradient.transpose(),self.residuals[:,oneiter]))
+
+            self.errors[oneiter+1] = np.linalg.norm(self.lhs(xNew))
+            if self.errors[oneiter+1] > self.errors[oneiter]:
+                self.omegas[oneiter+1] = self.omegas[oneiter+1]*5
+                self.estimates[:,oneiter+1] = xOld
+                self.residuals[:,oneiter+1] = self.lhs(xOld)
+            else :
+                self.omegas[oneiter+1] = self.omegas[oneiter+1]/5
+                self.estimates[:,oneiter+1] = xNew
+                xOld = xNew
+                self.residuals[:,oneiter+1] = self.lhs(xNew)
+
+        self.xstar = xNew
+
+        return xNew
+
